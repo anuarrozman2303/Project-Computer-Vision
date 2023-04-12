395a2e83cd86855f0041b4f9ee8fd7a4394cc533
Anuar Rozman <anuarrozman2303@gmail.com>
2023-04-12 16:21:55 +0800

    Solved.

--- Camera Calibration/camera_calibration.py
+++ Camera Calibration/camera_calibration.py
@@ -1,51 +1,48 @@
 import numpy as np
-import cv2
+import cv2 as cv
+import glob
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('Camera Calibration\\*.jpg')
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
 
-# Define the 3D object points (coordinates of points on calibration pattern)
-object_points = []
-for i in range(6):
-    for j in range(8):
-        object_points.append([j, i, 0]) # assuming z=0 as the calibration pattern lies on a plane
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-object_points = np.array(object_points, dtype=np.float32)
+img = cv.imread('Camera Calibration\\left12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-# Initialize arrays to store object points and image points from multiple images
-object_points_list = []
-image_points_list = []
+# Guna Undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('Camera Calibration\\Undistort.png', dst)
 
-# Load and process multiple images for calibration
-for i in range(1, 11):  # assuming 10 calibration images named as 'calibration1.jpg', 'calibration2.jpg', ...
-    image = cv2.imread("calib_radial.jpg")
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Guna Remapping
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('Camera Calibration\\Remapping.png', dst)
 
-    # Detect chessboard corners in the image
-    ret, corners = cv2.findChessboardCorners(gray, (8, 6), None)
-
-    if ret:  # if corners are detected
-        object_points_list.append(object_points)
-        image_points_list.append(corners)
-
-# Perform camera calibration
-ret, camera_matrix, distortion_coeffs, rvecs, tvecs = cv2.calibrateCamera(object_points_list, image_points_list,
-                                                                          gray.shape[::-1], None, None)
-
-# Get optimal new camera matrix
-img = cv2.imread('calib_radial.jpg')
-h, w = img.shape[:2]
-new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coeffs, (w, h), 1, (w, h))
-
-# Compute undistortion maps
-mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, distortion_coeffs, None, new_camera_matrix, (w, h), 5)
-
-# Undistort the example image
-undistorted_image = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
-
-np.savez('B.npz', mtx=camera_matrix, dist=distortion_coeffs, rvecs=rvecs, tvecs=tvecs)
-
-# Display original and undistorted image side by side
-cv2.imshow("Original Image", img)
-cv2.imshow("Undistorted Image", undistorted_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+np.savez('B.npz', mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
